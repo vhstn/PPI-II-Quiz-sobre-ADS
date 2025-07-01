82c2b8bca22bb3748e5589702a79f02a1d05371a
Vinicius Lanser <vini.lanser@gmail.com>
2025-07-01 18:25:40 -0300

    Mensagem ao realizar logout

--- master/logout.php
+++ master/logout.php
@@ -1,6 +1,13 @@
 <?php
     include_once 'processamento/session_manager.php';
     
-    SessionManager::destroy();    
+    SessionManager::start();
+
+    SessionManager::setFlashMessage([
+        'icon' => 'success',
+        'message' => 'Logout efetuado com sucesso!',
+        'isLogoutRequest' => true
+    ]);
+      
     header('Location: index.php#login');
 ?>
